03f0e6a5ee519ebafe6b4372f35c0cfb08ca14c9
Kyle Donahue <kyle.donahue@gmail.com>
2015-01-09 17:14:19 -0500

    Added some changes to the geometry of the window.

--- captioner.py
+++ captioner.py
@@ -3,6 +3,7 @@ import os
 import sys
 import glob
 import Tkinter
+import tkMessageBox
 import threading
 from PIL import Image
 from PIL import ImageTk
@@ -18,12 +19,17 @@ def updateImage(root, filename):
 
     # Draw the new image
     root.title("Captioner 0.1a: " + filename)
-    root.label.configure(image=tkimage)
-    root.label.image = tkimage
-    root.label.pack()
+    root.toplabel.configure(image=tkimage)
+    root.toplabel.image = tkimage
+    root.toplabel.pack()
 
 def waitForInput():
     root.input_ready.wait()
+
+    # Check to see if we were woken up by the window close event.
+    if root.closed:
+        sys.exit(0)
+
     root.input_ready.clear()
     return root.entry.get()
 
@@ -41,7 +47,7 @@ def CaptionApp(root, files):
         root.bind("<<NextImage>>", handler)
         root.event_generate("<<NextImage>>")
 
-        # Get caption from command line and add it to the metadata.
+        # Get caption from our text box. and add it to the metadata.
         comment = waitForInput()
         root.entry.delete(0, Tkinter.END)
         source = pexif.JpegFile.fromFile(infile)
@@ -51,28 +57,66 @@ def CaptionApp(root, files):
 
         source.writeFile(base_fname + "_captioned" + ext)
 
+    # We have processed all the files.  Now close our window.
     root.quit()
+
+def onClose():
+    if DEBUG:
+        root.closed = True
+        root.input_ready.set()
+        root.destroy()
+        root.quit()
+        sys.exit(0)
+        
+    if tkMessageBox.askokcancel(
+            "Quit?", "Are you sure you want to quit?"):
+        root.input_ready.set()
+        root.closed = True
+        root.destroy()
+        root.quit()        
+        sys.exit(0)
 
 if len(sys.argv) > 1:
     path = sys.argv[1]
 else:
     path = os.getcwd() + '/'
 
+
+DEBUG = True
 # Get a list of all JPEG files in directory
 files = glob.glob(path + '*.jpg')
 
+if len(files) == 0:
+    print 'No JPEG files found in ' + path
+    sys.exit(0)
+
 # Set up our window.
 root = Tkinter.Tk()
+
+# Create an event for communication between data editing thread
+# and the window thread so we know to write out the caption
 root.input_ready = threading.Event()
-root.label = Tkinter.Label(root)
-root.label.pack()
-root.entry = Tkinter.Entry(root, width=50)
-root.entry.pack()
-root.button = Tkinter.Button(root,
+root.closed = False
+
+root.toplabel = Tkinter.Label(root)
+root.toplabel.pack()
+root.bottomlabel = Tkinter.Label(root)
+root.bottomlabel.pack()
+root.bottomlabel.entry = Tkinter.Entry(root.bottomlabel, width=50)
+root.bottomlabel.entry.grid(row=0)
+
+# Bind pressing Enter while in the text field to accept the caption
+# Use a lambda binding to discard the event argument as it is not needed
+root.bottomlabel.entry.bind('<Return>', lambda x: root.input_ready.set())
+
+root.bottomlabel.button = Tkinter.Button(root.bottomlabel,
                              text='Ok',
                              width=10,
                              command=root.input_ready.set)
-root.button.pack()
+root.bottomlabel.button.grid(row=0, column=1)
+
+# Set up a handler for the window close event
+root.protocol("WM_DELETE_WINDOW", onClose)
 
 # Run the captioning thread.
 display = threading.Thread(target=CaptionApp, args=(root, files))
